abfb12b58be37c9aa4255a00b6041b56d47f1c33
Paul Herman <pmherman85@icloud.com>
2018-12-05 15:06:09 -0500

    no message

--- src/components/Actors/Actors.js
+++ src/components/Actors/Actors.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
-import { Grid, Col } from 'react-bootstrap'
+import { Grid, Col, Row, Image, ListGroup, Thumbnail } from 'react-bootstrap';
+import './Actors.css';
 
 export default class Actors extends Component {
     render() {
         return (
             <Grid className='content'>
-                <Col xs={12} md={6}>
-                    <h1 className='heading'>The Stars:</h1>
-                </Col>
+                <Row>
+                    <Col xs={12} md={12}>
+                        <h1 className='heading'>The Stars:</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <ListGroup>
+                    <Col xs={12} lg={4}>
+                    <Thumbnail src='https://i.ytimg.com/vi/gQz-RaBStNQ/hqdefault.jpg' alt='Red Hero'>
+                        <h3>Red Guardian</h3>
+                    </Thumbnail>
+                    <Thumbnail src='http://news.tokunation.com/wp-content/uploads/sites/5/2017/05/Shishiranger.jpg' alt='Green Hero'>
+                        <h3>Green Guardian</h3>
+                    </Thumbnail>
+                    </Col>
+                    <Col xs={12} lg={4}>
+                        <Thumbnail src='https://vignette.wikia.nocookie.net/powerrangers/images/a/a2/TenmaRanger.jpg/revision/latest?cb=20110122000203' alt='Blue Hero'>
+                            <h3>Blue Guardian</h3>
+                        </Thumbnail>
+                    </Col>
+                    <Col xs={12} lg={4}>
+                        <Thumbnail src='https://vignette.wikia.nocookie.net/powerrangers/images/1/1f/DairaYellow.jpg/revision/latest?cb=20110503193534' alt='Yellow Hero'>
+                            <h3>Yellow Guardian</h3>
+                        </Thumbnail>      
+                    </Col>
+                    <Col xs={12} lg={4}>
+                        <Thumbnail src='https://iv1.lisimg.com/image/10071919/720full-rin-%28dairanger%29.jpg' alt='Pink Hero'>
+                            <h3>Pink Guardian</h3>
+                        </Thumbnail>      
+                    </Col>
+                    </ListGroup>
+                </Row>
             </Grid> 
         )
     }
